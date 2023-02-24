5ab0cc3669e51484a5c115c10fd75e864ffff540
Maxwell Tsu <maxtsu@juniper.net>
2023-02-24 17:57:13 +0000

    test

--- main.go
+++ main.go
@@ -10,66 +10,67 @@ import (
 )
 
 func main() {
-        fmt.Println("Hello World!")
-        var broker= "10.54.162.129:9092"
-        var topics= []string{"cisco"}
-        var group= "healthbot"
-        sigchan := make(chan os.Signal, 1)
-        signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("Hello World!")
+	var broker= "10.54.162.129:9092"
+	var topics= []string{"cisco"}
+	var group= "healthbot"
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
-        consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-                "bootstrap.servers": broker,
-                "broker.address.family": "v4",
-                "group.id":              group,
-                "session.timeout.ms":    6000,
-                // Start reading from the first message of each assigned
-                // partition if there are no previously committed offsets
-                // for this group.
-                "auto.offset.reset": "earliest",
-                // Whether or not we store offsets automatically.
-                "enable.auto.offset.store": false,
-        })
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers": broker,
+			"broker.address.family": "v4",
+			"group.id":              group,
+			"session.timeout.ms":    6000,
+			// Start reading from the first message of each assigned
+			// partition if there are no previously committed offsets
+			// for this group.
+			"auto.offset.reset": "earliest",
+			// Whether or not we store offsets automatically.
+			"enable.auto.offset.store": false,
+	})
 
-        if err != nil {
-                fmt.Println("Failed to create consumer: %s\n", err)
-        }
-        fmt.Printf("Created Consumer %v\n", consumer)
+	if err != nil {
+			fmt.Println("Failed to create consumer: %s\n", err)
+	}
+	fmt.Printf("Created Consumer %v\n", consumer)
 
-        err = consumer.SubscribeTopics(topics, nil)
+	err = consumer.SubscribeTopics(topics, nil)
 
-        run := true
-        for run {
-                select {
-                case sig := <-sigchan:
-                        fmt.Printf("Caught signal %v: terminating\n", sig)
-                        run = false
-                default:
-                        ev := consumer.Poll(100)
-                        if ev == nil {
-                                continue
-                        }
-                        switch e := ev.(type) {
-                        case *kafka.Message:
-                                // Process the message received.
-                                fmt.Printf("%% Message on %s:\n%s\n",
-                                        e.TopicPartition, string(e.Value))
-                                if e.Headers != nil {
-                                        fmt.Printf("%% Headers: %v\n", e.Headers)
-                                }
-                                _, err := consumer.StoreMessage(e)
-                                if err != nil {
-                                        fmt.Fprintf(os.Stderr, "%% Error storing offset after message %s:\n",
-                                                e.TopicPartition)
-                                }
-                        case kafka.Error:
-                                fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
-                                if e.Code() == kafka.ErrAllBrokersDown {
-                                        run = false
-                                }
-                        default:
-                                fmt.Printf("Ignored %v\n", e)
-                }
-        }
-        fmt.Printf("Closing consumer\n")
-        consumer.Close()
+	run := true
+	for run {
+			select {
+			case sig := <-sigchan:
+					fmt.Printf("Caught signal %v: terminating\n", sig)
+					run = false
+			default:
+					ev := consumer.Poll(100)
+					if ev == nil {
+							continue
+					}
+					switch e := ev.(type) {
+					case *kafka.Message:
+							// Process the message received.
+							fmt.Printf("%% Message on %s:\n%s\n",
+									e.TopicPartition, string(e.Value))
+							if e.Headers != nil {
+									fmt.Printf("%% Headers: %v\n", e.Headers)
+							}
+							_, err := consumer.StoreMessage(e)
+							if err != nil {
+									fmt.Fprintf(os.Stderr, "%% Error storing offset after message %s:\n",
+											e.TopicPartition)
+							}
+					case kafka.Error:
+							fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
+							if e.Code() == kafka.ErrAllBrokersDown {
+									run = false
+							}
+					default:
+							fmt.Printf("Ignored %v\n", e)
+					}
+			}
+		}
+	fmt.Printf("Closing consumer\n")
+	consumer.Close()
 }
