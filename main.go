409f08a504a829bd6c8d30eafbc66d8d75348c2a
Maxwell Tsu <maxtsu@juniper.net>
2023-02-28 19:12:06 +0000

    g

--- main.go
+++ main.go
@@ -16,6 +16,50 @@ import (
 // var configfile = "/etc/byoi/config.json"
 var configfile = "config.json"
 
+// The Config struct for the config.json
+type Config struct {
+	Device   []Device `json:"device"`
+	Security Security `json:"security"`
+	KVs      []KVs    `json:"kvs"`
+	logger   log.Logger
+}
+
+// Device type
+type Device struct {
+	Name      string         `json:"name"`
+	SystemID  string         `json:"system-id"`
+	Sensor    []Sensor       `json:"sensor"`
+	HBStorage HBStorage      `json:"healthbot-storage"`
+	Auth      Authentication `json:"authentication"`
+}
+
+// Sensor config
+type Sensor struct {
+	Name        string `json:"name"`
+	KVs         []KVs  `json:"kvs"`
+	Measurement string `json:"measurement"`
+}
+
+// HBStorage to specify DB name and retention policy name for this device
+type HBStorage struct {
+	DB              string `json:"database"`
+	RetentionPolicy string `json:"retention-policy"`
+}
+
+// Authentication config
+type Authentication struct {
+}
+
+// Security config
+type Security struct {
+}
+
+// KVs config
+type KVs struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
 func testConfig() {
 	file, err := os.Open(configfile)
 	if err != nil {
@@ -26,7 +70,7 @@ func testConfig() {
 
 	byteResult, _ := ioutil.ReadAll(file)
 
-	var configuration hbinconfig.Device
+	var configuration Config
 	json.Unmarshal(byteResult, &configuration)
 	fmt.Println(configuration)
 }
