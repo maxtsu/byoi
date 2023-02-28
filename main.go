6eff656dba8fba899049d4d8d9181dbe20e32f6b
Maxwell Tsu <maxtsu@juniper.net>
2023-02-28 21:58:18 +0000

    j

--- main.go
+++ main.go
@@ -6,10 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 // var configfile = "/etc/byoi/config.json"
@@ -87,84 +83,15 @@ func configJSON() {
 	//fmt.Println(res)
 
 	// we initialize our Users array
-	var users Top
+	var configuration Top
 	// we unmarshal our byteArray which contains our
 	// jsonFile's content into 'users' which we defined above
-	json.Unmarshal(byteResult, &users)
-	fmt.Println(users.Hbin)
-}
-
-func btkafka(broker string, topics []string, group string) {
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-
-	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers":     broker,
-		"broker.address.family": "v4",
-		"group.id":              group,
-		"session.timeout.ms":    6000,
-		// Start reading from the first message of each assigned
-		// partition if there are no previously committed offsets
-		// for this group.x
-		"auto.offset.reset": "earliest",
-		// Whether or not we store offsets automatically.
-		"enable.auto.offset.store": false,
-	})
-
-	if err != nil {
-		fmt.Println("Failed to create consumer: %s\n", err)
-	}
-	fmt.Printf("Created Consumer %v\n", consumer)
-
-	err = consumer.SubscribeTopics(topics, nil)
-
-	run := true
-	for run {
-		select {
-		case sig := <-sigchan:
-			fmt.Printf("Caught signal %v: terminating\n", sig)
-			run = false
-		default:
-			ev := consumer.Poll(100)
-			if ev == nil {
-				continue
-			}
-			switch e := ev.(type) {
-			case *kafka.Message:
-				// Process the message received.
-				fmt.Printf("%% Message on %s:\n%s\n",
-					e.TopicPartition, string(e.Value))
-				if e.Headers != nil {
-					fmt.Printf("%% Headers: %v\n", e.Headers)
-				}
-				_, err := consumer.StoreMessage(e)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "%% Error storing offset after message %s:\n",
-						e.TopicPartition)
-				}
-			case kafka.Error:
-				fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
-				if e.Code() == kafka.ErrAllBrokersDown {
-					run = false
-				}
-			default:
-				fmt.Printf("Ignored %v\n", e)
-			}
-		}
-	}
-	fmt.Printf("Closing consumer\n")
-	consumer.Close()
+	json.Unmarshal(byteResult, &configuration)
+	fmt.Println(configuration)
 }
 
 func main() {
-	fmt.Println("Hello World!")
-	//var broker = "10.54.162.129:9092"
-	//var topics = []string{"cisco"}
-	//var group = "healthbot"
-
 	//read config file
 	configJSON()
 
-	//open connection to kafka broker
-	// btkafka(broker, topics, group)
 }
