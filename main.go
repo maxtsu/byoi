ba963f0e93b0c23fc46dfff7a175a35b8e5b3ade
Maxwell Tsu <maxtsu@juniper.net>
2023-03-03 14:22:24 +0000

    s

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ package main
 // influxdb point write
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	//"maxwell/configjson"
 )
 
@@ -14,9 +16,31 @@ import (
 // var configfile = "/etc/byoi/config.json"
 //var configfile = "config.json"
 
+// TEST struct
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
 	fmt.Println("HelloWorld!")
 	//configjson.ConfigJSON(configfile)
 	//fmt.Println(configjson.Configuration.Hbin.Inputs[0].Plugin.Config.Device[1])
 
+	//TESTING json writing
+	file, err := os.Open("data.json")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	defer file.Close()
+	var p Person
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&p)
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	fmt.Println(p)
 }
