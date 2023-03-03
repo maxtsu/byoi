641b7bc224e536914c6da4b321034056da6f9c41
Maxwell Tsu <maxtsu@juniper.net>
2023-03-03 14:04:47 +0000

    gf

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
+// Program calls the 3 components for the healthbot plugin
+// Kafka module for ingesting data
+// configjson for reading the /etc/byoi/config.json
+// influxdb point write
+
 import (
 	"fmt"
-	"maxwell/configjson"
+	//"maxwell/configjson"
 )
 
 // the plugin config.json file
 // var configfile = "/etc/byoi/config.json"
-var configfile = "config.json"
+//var configfile = "config.json"
 
 func main() {
 	fmt.Println("HelloWorld!")
-	configjson.ConfigJSON(configfile)
-	fmt.Println(configjson.Configuration.Hbin.Inputs[0].Plugin.Config.Device[1])
+	//configjson.ConfigJSON(configfile)
+	//fmt.Println(configjson.Configuration.Hbin.Inputs[0].Plugin.Config.Device[1])
+
 }
