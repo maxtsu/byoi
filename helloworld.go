70d9121c6f3788e4b7d57bc1635a747b467dc301
Maxwell Tsu <maxtsu@juniper.net>
2023-03-25 15:08:55 +0000

    bin1

--- helloworld.go
+++ helloworld.go
@@ -3,8 +3,12 @@ package main
 // Test program for linux compile
 import (
 	"fmt"
+	"maxwell/configjson"
 )
 
 func main() {
 	fmt.Println("HelloWorld!")
+	configjson.ConfigJSON("config.json")
+
+	fmt.Println("hbDB: " + configjson.Configuration.Hbin.Inputs[0].Plugin.Config.Device[0].HBStorage.DB)
 }
