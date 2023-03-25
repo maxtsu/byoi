7502b3125f2325114e19b244e29a397aa1cd3b73
Maxwell Tsu <maxtsu@juniper.net>
2023-03-25 15:33:19 +0000

    bin1

--- helloworld.go
+++ helloworld.go
@@ -4,6 +4,8 @@ package main
 import (
 	"fmt"
 	"maxwell/configjson"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
@@ -11,4 +13,22 @@ func main() {
 	configjson.ConfigJSON("config.json")
 
 	fmt.Println("hbDB: " + configjson.Configuration.Hbin.Inputs[0].Plugin.Config.Device[0].HBStorage.DB)
+
+	var broker = "10.54.162.129:9092"
+	var topics = []string{"cisco"}
+	var group = "healthbot"
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":     broker,
+		"broker.address.family": "v4",
+		"group.id":              group,
+		"session.timeout.ms":    6000,
+		// Start reading from the first message of each assigned
+		// partition if there are no previously committed offsets
+		// for this group.x
+		"auto.offset.reset": "earliest",
+		// Whether or not we store offsets automatically
+		"enable.auto.offset.store": false,
+	})
+
 }
