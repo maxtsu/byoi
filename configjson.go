95e840da038a64d20ef8a39cb80e13d7b90154f6
Maxwell Tsu <maxtsu@juniper.net>
2023-03-28 20:55:53 +0100

    today

--- configjson.go
+++ configjson.go
@@ -1,8 +1,5 @@
 package configjson
 
-// Package accepts json config file
-// Will extract to a struct
-
 import (
 	"encoding/json"
 	"fmt"
@@ -61,7 +58,7 @@ type Plugin struct {
 type Config struct {
 	Device []Device `json:"device"`
 	Devgrp string   `json:"device-group"`
-	KVs    []KVs    `json:"kvs"`
+	KVS    []KVS    `json:"kvs"`
 }
 
 // Device type
@@ -76,7 +73,7 @@ type Device struct {
 // Sensor config
 type Sensor struct {
 	Name        string `json:"name"`
-	KVs         []KVs  `json:"kvs"`
+	KVS         []KVS  `json:"kvs"`
 	Measurement string `json:"measurement"`
 }
 
@@ -97,10 +94,25 @@ type Password struct {
 	username string `json:"username"`
 }
 
-// KVs config
-type KVs struct {
+// KVS config
+type KVS struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
+}
+
+// struct defining device details
+//
+//	-SensorPath
+//	-deviceName
+//	-kvs_path
+//	-measurement
+//	-database
+type Device_item struct {
+	SensorPath  string
+	DeviceName  string
+	KVS_path    string
+	Measurement string
+	Database    string
 }
 
 var Configuration Top
@@ -118,4 +130,50 @@ func ConfigJSON(configfile string) {
 
 	byteResult, _ := ioutil.ReadAll(file)
 	json.Unmarshal(byteResult, &Configuration)
+}
+
+// Function to return array/slice of device details from config.json
+func GetDevices(devices []Device) []Device_item {
+	// create slice of devices
+	device_items := []Device_item{}
+	for _, a := range devices {
+		//extract list/slice of structs for sensors
+		keys := a.Sensor
+		for _, b := range keys {
+			// Create device_item struct and append it to the slice.
+			var d Device_item
+			d.DeviceName = a.SystemID
+			d.Database = a.HBStorage.DB
+			d.Measurement = b.Measurement
+			//extract the KVS pairs from sensor
+			keys_path := b.KVS
+			sensor_path := KVS_parsing(keys_path, []string{"path"})
+			d.SensorPath = sensor_path[0]
+			device_items = append(device_items, d)
+		}
+	}
+	return device_items
+}
+
+func KVS_parsing(keys []KVS, keyString []string) []string {
+	// Build a config map:
+	confMap := map[string]string{}
+	for _, v := range keys {
+		confMap[v.Key] = v.Value
+	}
+	// Find values by key in the config map
+	//Return the value of keys requested in same order
+	valueString := []string{}
+	for _, key := range keyString {
+		if v, ok := confMap[key]; ok {
+			value := v
+			//fmt.Println("keystring ", key)
+			//fmt.Println("value ", value)
+			valueString = append(valueString, value)
+		} else {
+			fmt.Println(key, "not in config.json")
+			valueString = append(valueString, "")
+		}
+	}
+	return valueString
 }
--- main.go
+++ main.go
@@ -8,6 +8,11 @@ package main
 import (
 	"fmt"
 	"maxwell/configjson"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 // the plugin config.json file
@@ -16,8 +21,47 @@ var configfile = "config.json"
 
 func main() {
 	fmt.Println("HelloWorld!")
+	//convert the config.json to a struct
 	configjson.ConfigJSON(configfile)
+	// extract the brokers and topics from the configjson KVS
+	brokertopic := configjson.KVS_parsing(configjson.Configuration.Hbin.Inputs[0].Plugin.Config.KVS, []string{"brokers", "topics"})
+	fmt.Println("this is the broker: " + brokertopic[0])
+	fmt.Println("this is the topics: " + brokertopic[1])
 
-	fmt.Println("hbDB: " + configjson.Configuration.Hbin.Inputs[0].Plugin.Config.Device[0].HBStorage.DB)
+	devices := configjson.Configuration.Hbin.Inputs[0].Plugin.Config.Device
+	// Call to get array of structs with device details
+	list_devices := configjson.GetDevices(devices)
+	fmt.Println("devices ")
+	fmt.Println(list_devices[0].DeviceName)
+	fmt.Println(list_devices[0].SensorPath)
+
+	bootstrapServers := brokertopic[0]
+	group := "byoi"
+	topics := brokertopic[1]
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": bootstrapServers,
+		// Avoid connecting to IPv6 brokers:
+		// This is needed for the ErrAllBrokersDown show-case below
+		// when using localhost brokers on OSX, since the OSX resolver
+		// will return the IPv6 addresses first.
+		// You typically don't need to specify this configuration property.
+		"broker.address.family": "v4",
+		"group.id":              group,
+		"session.timeout.ms":    6000,
+		// Start reading from the first message of each assigned
+		// partition if there are no previously committed offsets
+		// for this group.
+		"auto.offset.reset": "earliest",
+		// Whether or not we store offsets automatically.
+		"enable.auto.offset.store": false,
+	})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create consumer: %s\n", err)
+		os.Exit(1)
+	}
 
 }
