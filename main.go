cb0e2cf12da6908f5f6b1875f0cc32e24b368e9a
Maxwell Tsu <maxtsu@juniper.net>
2023-03-29 14:13:54 +0100

    w5

--- main.go
+++ main.go
@@ -33,11 +33,10 @@ func main() {
 	list_devices := configjson.GetDevices(devices)
 	fmt.Println("devices ")
 	fmt.Println(list_devices[0].DeviceName)
-	fmt.Println(list_devices[0].SensorPath)
 
 	bootstrapServers := brokertopic[0]
 	group := "byoi"
-	topics := brokertopic[1]
+	topics := []string{brokertopic[1]}
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
@@ -64,7 +63,55 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("Created Consumer %v\n", consumer)
-
 	err = consumer.SubscribeTopics(topics, nil)
-	//run := true
+
+	run := true
+	for run {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := consumer.Poll(100)
+			if ev == nil {
+				continue
+			}
+			switch message := ev.(type) {
+			case *kafka.Message:
+				// Process the message received.
+				fmt.Printf("%% Message on %s:\n%s\n",
+					message.TopicPartition, string(message.Value))
+				if message.Headers != nil {
+					fmt.Printf("%% Headers: %v\n", message.Headers)
+				}
+
+				// We can store the offsets of the messages manually or let
+				// the library do it automatically based on the setting
+				// enable.auto.offset.store. Once an offset is stored, the
+				// library takes care of periodically committing it to the broker
+				// if enable.auto.commit isn't set to false (the default is true).
+				// By storing the offsets manually after completely processing
+				// each message, we can ensure atleast once processing.
+				_, err := consumer.StoreMessage(message)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%% Error storing offset after message %s:\n",
+						message.TopicPartition)
+				}
+			case kafka.Error:
+				// Errors should generally be considered
+				// informational, the client will try to
+				// automatically recover.
+				// But in this example we choose to terminate
+				// the application if all brokers are down.
+				fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", message.Code(), message)
+				if message.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			default:
+				fmt.Printf("Ignored %v\n", message)
+			}
+		}
+	}
+	fmt.Printf("Closing consumer\n")
+	consumer.Close()
 }
