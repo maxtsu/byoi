f4d8c7f6ee78705481641bae2fb1a6d31af9053b
Maxwell Tsu <maxtsu@juniper.net>
2023-04-05 17:43:54 +0100

    w9

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"byoi/configjson"
 	"byoi/greeting"
 	"fmt"
 )
 
+var configfile = "config.json"
+
 func main() {
 	fmt.Println("HelloWorld!")
 	fmt.Println(greeting.WelcomeText)
+
+	//convert the config.json to a struct
+	configjson.ConfigJSON(configfile)
+	fmt.Println("finicxhed process config.json")
 
 }
