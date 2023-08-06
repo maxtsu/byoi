4805bd8072a2ba0f0b0f5b4533449f84c8b99f64
Maxwell Tsu <maxtsu@juniper.net>
2023-08-06 21:13:52 +0100

    f

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 
 	// Set logging level From config.json
 	loggingLevel := configuration.Logging.Level
+	var level int = 4
 	switch loggingLevel {
 	case "debug":
 		level = 5
@@ -46,196 +47,196 @@ func main() {
 		level = 2
 	default:
 		level = 4
-		// Initialize Logger
-		// Level 10 = panic, fatal, error, warn, info, debug, & trace
-		// Level 5 = panic, fatal, error, warn, info, & debug
-		// Level 4 = panic, fatal, error, warn, & info
-		// Level 3 = panic, fatal, error, & warn
-		// Level 2 = panic, fatal & error
-		// Level 1 = panic, fatal
-		log.EnableLevelsByNumber(level)
-		log.EnableFormattedPrefix()
-
-		// Load rules.json into struct
-		byteResult = gnfingest.ReadFile(rulesfile)
-		var r1 []gnfingest.RulesJSON
-		err = json.Unmarshal(byteResult, &r1)
-		if err != nil {
-			fmt.Println("Unmarshall error", err)
-		}
-		// create map of structs key=rule-id
-		var rules = make(map[string]gnfingest.RulesJSON)
-		for _, r := range r1 {
-			rules[r.RuleID] = r
-		}
-
-		//### Call function to extract kafka brokers and topics
-		//   brokers, topics, user, password, saslmech, secprotocol = brokerTopic(configFile)
-		// extract the brokers and topics from the configjson KVS
-		brokertopic := gnfingest.KVS_parsing(configuration.Hbin.Inputs[0].Plugin.Config.KVS, []string{"brokers", "topics", "user", "password", "saslmech", "secprotocol"})
-
-		fmt.Println("this is the broker: " + brokertopic[0])
-		fmt.Println("this is the topics: " + brokertopic[1])
-		fmt.Println("this is the user: " + brokertopic[2])
-		fmt.Println("this is the password: " + brokertopic[3])
-		fmt.Println("this is the saslmech: " + brokertopic[4])
-		fmt.Println("this is the secprotocol: " + brokertopic[5])
-
-		//list of devices configuration from configjson
-		bootstrapServers := brokertopic[0]
-
-		// Generate unique uuid for kafka group-id
-		uuid := Create_uuid()
-		group := string(uuid)
-		topics := []string{brokertopic[1]}
-		devices := configuration.Hbin.Inputs[0].Plugin.Config.Device
-		// config.json list of device key from values under sensor for searching messages
-		keys := []string{"path", "rule-id"}
-		device_keys := gnfingest.DeviceDetails(devices, keys)
-		fmt.Printf("Device-Keys %+v\n", device_keys)
-
-		sigchan := make(chan os.Signal, 1)
-		signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-
-		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-			"bootstrap.servers":  bootstrapServers,
-			"group.id":           group,
-			"session.timeout.ms": 6000,
-			// Start reading from the first message of each assigned
-			// partition if there are no previously committed offsets
-			// for this group.
-			"auto.offset.reset": "earliest",
-			// Whether or not we store offsets automatically.
-			"enable.auto.offset.store": false,
-		})
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to create consumer: %s\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("Created Consumer %v\n", consumer)
-		err = consumer.SubscribeTopics(topics, nil)
-
-		//run := true
-		run := false
-		for run {
-			fmt.Printf("waiting for kafka message\n")
-			time.Sleep(2 * time.Second)
-			select {
-			case sig := <-sigchan:
-				fmt.Printf("Caught signal %v: terminating\n", sig)
-				run = false
-			default:
-				// Poll the consumer for messages or events
-				m := gnfingest.Message{}
-				event := consumer.Poll(400)
-				if event == nil {
-					continue
-				}
-				switch e := event.(type) {
-				case *kafka.Message:
-					// Process the message received.
-					fmt.Printf("%% Message on %s:\n%s\n",
-						e.TopicPartition, string(e.Value))
-					kafkaMessage := string(e.Value)
-
-					//sp := get_source_prefix(kafkaMessage)
-
-					json.Unmarshal([]byte(kafkaMessage), &m)
-					fmt.Printf("message struct: %+v\n", m)
-
-					//Start matching message to configured rules
-					//m := message_root{}
-					for _, d := range device_keys {
-						fmt.Println("Device: ", d.DeviceName)
-						if (d.DeviceName == m.Source) && (d.KVS_path == m.Updates[0].Path) {
-							fmt.Printf("name and prefix match ")
-						}
-					}
-
-					if e.Headers != nil {
-						fmt.Printf("%% Headers: %v\n", e.Headers)
-					}
-					// We can store the offsets of the messages manually or let
-					// the library do it automatically based on the setting
-					// enable.auto.offset.store. Once an offset is stored, the
-					// library takes care of periodically committing it to the broker
-					// if enable.auto.commit isn't set to false (the default is true).
-					// By storing the offsets manually after completely processing
-					// each message, we can ensure atleast once processing.
-					_, err := consumer.StoreMessage(e)
-					if err != nil {
-						fmt.Fprintf(os.Stderr, "%% Error storing offset after message %s:\n",
-							e.TopicPartition)
-					}
-				case kafka.Error:
-					// Errors should generally be considered
-					// informational, the client will try to
-					// automatically recover.
-					// In this example we choose to terminate
-					// the application if all brokers are down.
-					fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
-					if e.Code() == kafka.ErrAllBrokersDown {
-						run = false
-					}
-				default:
-					fmt.Printf("Ignored %v\n", e)
-				}
-			}
-		}
-
-		// Processing sample message
-		//var sample = "Sample4.json"
-		//var sample = "Sample20.json"
-		var sample = "interface-state.json"
-		//var sample = "isis-1.json"
-		byteResult = gnfingest.ReadFile(sample)
-		// Unmarshal JSON message into struct
-		var kafkaMessage gnfingest.Message
-		err = json.Unmarshal(byteResult, &kafkaMessage)
-		if err != nil {
-			fmt.Println("Unmarshal error", err)
-		}
-
-		// Extract message source IP remove port number
-		messageSource := strings.Split(kafkaMessage.Source, ":")[0]
-		fmt.Println("source: %s", messageSource)
-		// Extract message path remove index values []
-		re := regexp.MustCompile("[[].*?[]]")
-		messagePath := re.ReplaceAllString(kafkaMessage.Updates[0].Path, "")
-		fmt.Println("path: %s", messagePath)
-		// Start matching message to configured rules
-		for _, d := range device_keys {
-			if (d.DeviceName == messageSource) && (d.KVS_path == messagePath) {
-				//extract rule-id
-				rule_id := d.KVS_rule_id
-				fmt.Printf("rule-id: %+v\n", rule_id)
-				// Extract rule from rules.json
-				for _, f1 := range rules[rule_id].Fields {
-					for _, f2 := range f1.Path {
-						fmt.Println("f2: %+v\n", f2)
-					}
-					//path := f1.Path
-					//fields := kafkaMessage.Updates.Values.State
-				}
-			}
-		}
-
-		// parse to extract path & indexes from "Path:" value in message
-		var result []string
-		var k1 = make(map[string][]gnfingest.KVS)
-		result, k1 = gnfingest.PathExtract(kafkaMessage.Updates[0].Path)
-		fmt.Println("path list: %+v\n", result)
-		fmt.Println("map of keys: %+v\n", k1)
-
-		Test_json_map(kafkaMessage.Updates[0].Values)
-
-		log.Info("this is info log")
-		log.Println("some interesting logging message")
-		log.Debug("debug log")
-		log.Debugf("debug f %+v", k1)
-		log.Error("error log")
-		log.Infoln("info ln")
 	}
+	// Initialize Logger
+	// Level 10 = panic, fatal, error, warn, info, debug, & trace
+	// Level 5 = panic, fatal, error, warn, info, & debug
+	// Level 4 = panic, fatal, error, warn, & info
+	// Level 3 = panic, fatal, error, & warn
+	// Level 2 = panic, fatal & error
+	// Level 1 = panic, fatal
+	log.EnableLevelsByNumber(level)
+	log.EnableFormattedPrefix()
+
+	// Load rules.json into struct
+	byteResult = gnfingest.ReadFile(rulesfile)
+	var r1 []gnfingest.RulesJSON
+	err = json.Unmarshal(byteResult, &r1)
+	if err != nil {
+		fmt.Println("Unmarshall error", err)
+	}
+	// create map of structs key=rule-id
+	var rules = make(map[string]gnfingest.RulesJSON)
+	for _, r := range r1 {
+		rules[r.RuleID] = r
+	}
+
+	//### Call function to extract kafka brokers and topics
+	//   brokers, topics, user, password, saslmech, secprotocol = brokerTopic(configFile)
+	// extract the brokers and topics from the configjson KVS
+	brokertopic := gnfingest.KVS_parsing(configuration.Hbin.Inputs[0].Plugin.Config.KVS, []string{"brokers", "topics", "user", "password", "saslmech", "secprotocol"})
+
+	fmt.Println("this is the broker: " + brokertopic[0])
+	fmt.Println("this is the topics: " + brokertopic[1])
+	fmt.Println("this is the user: " + brokertopic[2])
+	fmt.Println("this is the password: " + brokertopic[3])
+	fmt.Println("this is the saslmech: " + brokertopic[4])
+	fmt.Println("this is the secprotocol: " + brokertopic[5])
+
+	//list of devices configuration from configjson
+	bootstrapServers := brokertopic[0]
+
+	// Generate unique uuid for kafka group-id
+	uuid := Create_uuid()
+	group := string(uuid)
+	topics := []string{brokertopic[1]}
+	devices := configuration.Hbin.Inputs[0].Plugin.Config.Device
+	// config.json list of device key from values under sensor for searching messages
+	keys := []string{"path", "rule-id"}
+	device_keys := gnfingest.DeviceDetails(devices, keys)
+	fmt.Printf("Device-Keys %+v\n", device_keys)
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  bootstrapServers,
+		"group.id":           group,
+		"session.timeout.ms": 6000,
+		// Start reading from the first message of each assigned
+		// partition if there are no previously committed offsets
+		// for this group.
+		"auto.offset.reset": "earliest",
+		// Whether or not we store offsets automatically.
+		"enable.auto.offset.store": false,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create consumer: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Created Consumer %v\n", consumer)
+	err = consumer.SubscribeTopics(topics, nil)
+
+	//run := true
+	run := false
+	for run {
+		fmt.Printf("waiting for kafka message\n")
+		time.Sleep(2 * time.Second)
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			// Poll the consumer for messages or events
+			m := gnfingest.Message{}
+			event := consumer.Poll(400)
+			if event == nil {
+				continue
+			}
+			switch e := event.(type) {
+			case *kafka.Message:
+				// Process the message received.
+				fmt.Printf("%% Message on %s:\n%s\n",
+					e.TopicPartition, string(e.Value))
+				kafkaMessage := string(e.Value)
+
+				//sp := get_source_prefix(kafkaMessage)
+
+				json.Unmarshal([]byte(kafkaMessage), &m)
+				fmt.Printf("message struct: %+v\n", m)
+
+				//Start matching message to configured rules
+				//m := message_root{}
+				for _, d := range device_keys {
+					fmt.Println("Device: ", d.DeviceName)
+					if (d.DeviceName == m.Source) && (d.KVS_path == m.Updates[0].Path) {
+						fmt.Printf("name and prefix match ")
+					}
+				}
+
+				if e.Headers != nil {
+					fmt.Printf("%% Headers: %v\n", e.Headers)
+				}
+				// We can store the offsets of the messages manually or let
+				// the library do it automatically based on the setting
+				// enable.auto.offset.store. Once an offset is stored, the
+				// library takes care of periodically committing it to the broker
+				// if enable.auto.commit isn't set to false (the default is true).
+				// By storing the offsets manually after completely processing
+				// each message, we can ensure atleast once processing.
+				_, err := consumer.StoreMessage(e)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%% Error storing offset after message %s:\n",
+						e.TopicPartition)
+				}
+			case kafka.Error:
+				// Errors should generally be considered
+				// informational, the client will try to
+				// automatically recover.
+				// In this example we choose to terminate
+				// the application if all brokers are down.
+				fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
+				if e.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			default:
+				fmt.Printf("Ignored %v\n", e)
+			}
+		}
+	}
+
+	// Processing sample message
+	//var sample = "Sample4.json"
+	//var sample = "Sample20.json"
+	var sample = "interface-state.json"
+	//var sample = "isis-1.json"
+	byteResult = gnfingest.ReadFile(sample)
+	// Unmarshal JSON message into struct
+	var kafkaMessage gnfingest.Message
+	err = json.Unmarshal(byteResult, &kafkaMessage)
+	if err != nil {
+		fmt.Println("Unmarshal error", err)
+	}
+
+	// Extract message source IP remove port number
+	messageSource := strings.Split(kafkaMessage.Source, ":")[0]
+	fmt.Println("source: %s", messageSource)
+	// Extract message path remove index values []
+	re := regexp.MustCompile("[[].*?[]]")
+	messagePath := re.ReplaceAllString(kafkaMessage.Updates[0].Path, "")
+	fmt.Println("path: %s", messagePath)
+	// Start matching message to configured rules
+	for _, d := range device_keys {
+		if (d.DeviceName == messageSource) && (d.KVS_path == messagePath) {
+			//extract rule-id
+			rule_id := d.KVS_rule_id
+			fmt.Printf("rule-id: %+v\n", rule_id)
+			// Extract rule from rules.json
+			for _, f1 := range rules[rule_id].Fields {
+				for _, f2 := range f1.Path {
+					fmt.Println("f2: %+v\n", f2)
+				}
+				//path := f1.Path
+				//fields := kafkaMessage.Updates.Values.State
+			}
+		}
+	}
+
+	// parse to extract path & indexes from "Path:" value in message
+	var result []string
+	var k1 = make(map[string][]gnfingest.KVS)
+	result, k1 = gnfingest.PathExtract(kafkaMessage.Updates[0].Path)
+	fmt.Println("path list: %+v\n", result)
+	fmt.Println("map of keys: %+v\n", k1)
+
+	Test_json_map(kafkaMessage.Updates[0].Values)
+
+	log.Info("this is info log")
+	log.Println("some interesting logging message")
+	log.Debug("debug log")
+	log.Debugf("debug f %+v", k1)
+	log.Error("error log")
+	log.Infoln("info ln")
 }
 
 func Test_json_map(rawdata json.RawMessage) {
