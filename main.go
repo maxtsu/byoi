f94e85fc949f4e3d3fbcfdbca239a39c1417775f
Maxwell Tsu <maxtsu@juniper.net>
2023-08-17 17:32:41 +0100

    g

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ import (
 	"github.com/gologme/log"
 )
 
-var configfile = "config.json"
+//var configfile = "config.json"
 
-// var configfile = "/etc/byoi/config.json"
+var configfile = "/etc/byoi/config.json"
 var rulesfile = "rules.json"
 
 func main() {
