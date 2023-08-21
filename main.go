ecefbfa570cdf0140dbd89c0b45f278959724101
Maxwell Tsu <maxtsu@juniper.net>
2023-08-21 17:42:37 +0100

    df

--- main.go
+++ main.go
@@ -124,7 +124,7 @@ func main() {
 			//run = false
 		default:
 			// Poll the consumer for messages or events
-			message := gnfingest.Message{}
+			//message := gnfingest.Message{}
 			event := consumer.Poll(400)
 			if event == nil {
 				continue
