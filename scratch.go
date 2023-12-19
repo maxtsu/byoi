dab49bcc97e0dbf6199fb1cc8d6959e48165db32
Maxwell Tsu <maxtsu@juniper.net>
2023-12-19 15:29:14 +0000

    2

--- scratch.go
+++ scratch.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/gologme/log"
+	"github.com/influxdata/influxdb/client/v2"
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
@@ -37,24 +38,26 @@ func main() {
 	}
 	batchPoint.AddPoint(pt)
 
+	InfluxDB2()
+
 	// Write the batch test
-	err = tandClient.Write(batchPoint)
+	/*err = tandClient.Write(batchPoint)
 	if err != nil {
 		fmt.Println("Write Error: ", err.Error())
 	} else {
 		fmt.Println("Succesful write")
-	}
+	}*/
 }
 
 // create InfluxDB v1.8 client
 func InfluxdbClient(tand_host string, tand_port string) client.Client {
 	url := "http://" + tand_host + ":" + tand_port
-	config := client.HTTPConfig{Addr: url}
+	//config := client.HTTPConfig{Addr: url}
+	//c, err := client.NewHTTPClient(config)
 	// create client
-	/*c, err := client.NewHTTPClient(client.HTTPConfig{
+	c, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr: url,
-	})*/
-	c, err := client.NewHTTPClient(config)
+	})
 	if err != nil {
 		log.Errorf("Error creating InfluxDB Client: ", err)
 	}
@@ -72,4 +75,49 @@ func DatabaseBp(database string) client.BatchPoints {
 	})
 	//fmt.Printf("Client create with BP %+v %+v", bp, c)
 	return bp
+}
+
+func InfluxDB2() {
+	// Create a new HTTPClient
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	// Create a new point batch
+	MyDB := "hb-default:cisco:cisco-B"
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  MyDB,
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a point and add to batch
+	tags := map[string]string{"cpu": "cpu-total"}
+	fields := map[string]interface{}{
+		"idle":   10.1,
+		"system": 53.3,
+		"user":   46.6,
+	}
+
+	pt, err := client.NewPoint("cpu_usage", tags, fields, time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+	bp.AddPoint(pt)
+
+	// Write the batch
+	if err := c.Write(bp); err != nil {
+		log.Fatal(err)
+	}
+
+	// Close client resources
+	if err := c.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
