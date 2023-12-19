24a6b4d5877fde9386bce61986f980ec8f872fc9
Maxwell Tsu <maxtsu@juniper.net>
2023-12-19 10:52:01 +0000

    logs

--- scratch.go
+++ scratch.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/gologme/log"
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
@@ -33,6 +34,38 @@ func main() {
 	destination = destination[1:]
 	fmt.Println("1: source %s", source)
 	fmt.Println("1: destination %s", destination)
+
+	// connect influxDB create Influx client return batchpoint
+	tand_host := "localhost"
+	tand_port := "8086"
+	Database := "hb-default:cisco:cisco-B"
+	batchPoint, client := influxdbClient(tand_host, tand_port, Database)
+
+	fmt.Printf("Client create with BP %+v\n", batchPoint)
+	fmt.Printf("Client create with client %+v\n", client)
+
+}
+
+// create InfluxDB v1.8 client
+func InfluxdbClient(tand_host string, tand_port string, database string) (client.BatchPoints, client.Client) {
+	url := "https://" + tand_host + ":" + tand_port
+	// create client
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: url,
+	})
+	if err != nil {
+		log.Errorf("Error creating InfluxDB Client: ", err)
+	}
+	defer c.Close()
+	log.Infof("Created InfluxDB Client: %+v\n", c)
+
+	// Create a new point batch for database
+	bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
+		Database: database,
+		//Precision: "s",
+	})
+	//fmt.Printf("Client create with BP %+v %+v", bp, c)
+	return bp, c
 }
 
 // start a new client
