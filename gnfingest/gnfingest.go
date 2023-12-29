2c0d017f17e6ebe410d082c5f4b4c815352c2ac9
Maxwell Tsu <maxtsu@juniper.net>
2023-12-29 17:43:48 +0000

    ergeg

--- gnfingest/gnfingest.go
+++ gnfingest/gnfingest.go
@@ -262,7 +262,7 @@ func (dev *Device_Details) FlushPoints() {
 	if error != nil {
 		log.Errorf("Device %s Create BatchPoint error: %s\n", dev.DeviceName, error.Error())
 	}
-	fmt.Printf("ERROR: %+v\n", error.Error())
+	fmt.Printf("ERROR: %+v\n", error)
 	fmt.Printf("BATCHPOINT: %+v\n", batchPoint)
 	pts := dev.Points
 	batchPoint.AddPoints(pts)
